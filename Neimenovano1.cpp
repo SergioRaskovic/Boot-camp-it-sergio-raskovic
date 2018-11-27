c0fb4620bcb4ca0461827cc3ac01e15e8cee0f29
SergioRaskovic <45363238+SergioRaskovic@users.noreply.github.com>
2018-11-27 09:27:49 +0100

    hello world
    1 test

---
+++ Neimenovano1.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // biblioteka podataka
+
+using namespace std; // nemoram pisati std stalno
+
+int main () //poslije njega pise
+{
+	
+	cout <<"Hello World!"<< endl; // ovo mi se pise 
+	system("PAUSE"); // da ostane text vise od sekunde
+	return 0; // podatak je tocan
+}
+
+
+
