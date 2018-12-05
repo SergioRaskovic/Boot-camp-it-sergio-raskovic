bc33bc43b20fe0bc7075c5f4585479ef8da7ca4b
SergioRaskovic <45363238+SergioRaskovic@users.noreply.github.com>
2018-12-05 15:53:03 +0100

    ć

---
+++ Sergio Raskovic/bla.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+
+	for (int i = 0; a > i; i++) {
+		cout << i;
+	}
+
+
+    
+}
